bfd7ef7b69baf1ed985db7fe37872d5bf30c716d
maielfth <165782420+maielfth@users.noreply.github.com>
2024-05-15 15:45:20 +0700

    first commit

---
+++ app.js
@@ -0,0 +1,14 @@
+let names = ['Sofia','David','Juan'];
+console.log(names);
+
+names.push('Sara','Augustin');
+console.log(names);
+
+names.shift('');
+console.log(names);
+
+names.splice(1,0,'Renata');
+console.log(names);
+
+names.push('Elena');
+console.log(names);
